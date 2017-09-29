6cc92cb59355c41c33292713d7c65cddd6bbcdfd
dvanorny <dvanorny@imagetrend.com>
2017-09-29 08:32:35 -0500

    Style changes; downloaded some google API Nuget packages

--- Code/LiveScores/App_Start/BundleConfig.cs
+++ Code/LiveScores/App_Start/BundleConfig.cs
@@ -11,25 +11,24 @@ namespace LiveScores
             RegisterScripts(bundles);
             RegisterStyles(bundles);
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/SiteBundle").Include("~/Content/site.css"));
         }
 
         private static void RegisterScripts(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/Scripts/JQuery/Bundle").Include("~/Scripts/JQuery/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/Bootstrap/Bundle").Include("~/Scripts/Bootstrap/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/JQueryUI/Bundle").Include("~/Scripts/JQueryUI/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/MvcGrid/Bundle").Include("~/Scripts/MvcGrid/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/Shared/Bundle").Include("~/Scripts/Shared/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/JQuery/JQueryBundle").Include("~/Scripts/JQuery/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Bootstrap/BootstrapBundle").Include("~/Scripts/Bootstrap/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/JQueryUI/JQueryBundle").Include("~/Scripts/JQueryUI/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/MvcGrid/MvcGridBundle").Include("~/Scripts/MvcGrid/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Shared/SharedBundle").Include("~/Scripts/Shared/*.js"));
 		}
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/JQueryUI/Bundle").Include("~/Content/JQueryUI/*.css"));
-            bundles.Add(new StyleBundle("~/Content/Bootstrap/Bundle").Include("~/Content/Bootstrap/*.css"));
-            bundles.Add(new StyleBundle("~/Content/FontAwesome/Bundle").Include("~/Content/FontAwesome/*.css"));
-            bundles.Add(new StyleBundle("~/Content/MvcGrid/Bundle").Include("~/Content/MvcGrid/*.css"));
-            bundles.Add(new StyleBundle("~/Content/Shared/Bundle").Include("~/Content/Shared/*.css"));
+            bundles.Add(new StyleBundle("~/Content/JQueryUI/JQueryStyleBundle").Include("~/Content/JQueryUI/*.css"));
+            bundles.Add(new StyleBundle("~/Content/Bootstrap/BootstrapStyleBundle").Include("~/Content/Bootstrap/*.css"));
+            bundles.Add(new StyleBundle("~/Content/FontAwesome/FontAwesomeBundle").Include("~/Content/FontAwesome/*.css"));
+            bundles.Add(new StyleBundle("~/Content/MvcGrid/MvcGridStyleBundle").Include("~/Content/MvcGrid/*.css"));
+            bundles.Add(new StyleBundle("~/Content/Shared/SharedStyleBundle").Include("~/Content/Shared/*.css"));
 		}
     }
 }
